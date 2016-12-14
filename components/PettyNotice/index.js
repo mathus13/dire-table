9c6852a92d08cafa0f46d8187f5be998e8a5afab
Frank Albenesius <frankalbenesius@gmail.com>
2016-12-13 23:50:55 -0600

    add petty notice for jordan

---
+++ components/PettyNotice/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { style } from 'next/css'
+import { zIndexes, sizes, colors } from '../constants'
+
+const styles = style({
+  backgroundColor: 'Crimson',
+  position: 'absolute',
+  left: '5rem',
+  top: '5rem',
+  color: 'white',
+  padding: '1rem',
+  zIndex: zIndexes.hover,
+  fontSize: '1.2rem',
+  lineHeight: '1.5rem',
+  whiteSpace: 'pre',
+  fontFamily: "'PT Mono', monospace",
+  boxShadow: `0 0 ${sizes.shadow} ${colors.shadow}`,
+})
+const message =
+`Alright there's content here now, Jordan.
+    You can get off my ass.
+`
+export default () => (
+  <div className={styles}>{message}</div>
+)
--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,7 @@ export default class Index extends React.Component {
     return (
       <Provider store={this.store}>
         <Wrapper>
+          <PettyNotice />
           <Map />
           <Chat />
         </Wrapper>
